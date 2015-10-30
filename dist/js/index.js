c677c0af7802434631578e690ca07a81aed14ccc
gloryfades <vietphuynh@gmail.com>
2015-10-30 00:57:01 -0700

    test

---
+++ dist/js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(e) {
+	// Parse.initialize("xoKIhEIxEK5SxBRVMcucXhjd2wzFwMKGwSUoynot", "fXKCq8wQgTcOpoBkRhzf2DUZDofbdFp9bM8IL1Eo");
+	$('.selectpicker').selectpicker({
+		style: 'btn-info',
+		size: 4
+	});
+	// var dropdown = document.getElementByClassName("form-control");
+	// if (dropdown){
+	// 	var Foundation = Parse.Object.extend("Foundation");
+	// 	var query =  new Parse.Query(Foundation);
+	// 	query.ascending("course");
+	// 	for
+	// }
+	// $('form-control')
+});
